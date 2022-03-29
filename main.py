56ab4fd90c83b94d2446b73f01518e2f3ac38294
Lenilson da Cruz Santiago <santiagolenil@gmail.com>
2022-03-28 21:17:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,13 +23,13 @@ atendimentos[0].iniciarAtendimento()
 atendimentos[0].finalizarAtendimento()
 
 
-atendimentos[0].gerarRelatorio('teste')
+print(atendimentos[0].gerarRelatorio('teste'))
 
-arrayDeTempo = ['00:08:21', '00:08:21', '00:08:12']
+#arrayDeTempo = ['00:08:21', '00:08:21', '00:08:12']
 #'00:12:50' => 12 x 60 = 720 + 50s restantes = 770s
 #'00:04:21' => 4 x 60 = 240 + 21s restantes = 261s
 #media = 770 + 261 = 1031s / 2 = 515.5s
 #media em minutos = 515.5 / 60s = 8.5 minutos
 #SERÁ QUE PODE ESTAR CERTO? 
-result = mediaTest(arrayDeTempo)
-print(str(result))
+#result = mediaTest(arrayDeTempo)
+#print(str(result))
